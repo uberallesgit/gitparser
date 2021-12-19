8808505bfc633a018d37f461920333ebe4a8d535
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 <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>
2021-12-20 00:13:19 +0300

    index file created

--- saa_parse.py
+++ saa_parse.py
@@ -3,14 +3,21 @@ from bs4 import BeautifulSoup
 import lxml
 
 
-headers = {
-    "Accept": "*/*",
-"User-Agent" :"Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
-}
-url = "http://mebel-saa.ru/"
+# headers = {
+#     "Accept": "*/*",
+# "User-Agent" :"Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
+# }
+# url = "http://mebel-saa.ru/"
+#
+# req = requests.get(url,headers=headers)
+# src = req.text
+# print(src)
+# with open("index.html","w") as file:
+#     file.write(src)
 
-req = requests.get(url,headers=headers)
-src = req.text
-print(src)
-with open("index.html","w") as file:
-    file.write(src)
+with open("index.html") as files:
+    src = file.read()
+
+
+
+
