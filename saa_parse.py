4f7e257d48e11a81ba2972d82afa9d670477ffa9
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 <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>
2021-12-20 00:03:22 +0300

    first commit

--- saa_parse.py
+++ saa_parse.py
@@ -1,6 +1,12 @@
 import requests
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import lxml
 
+
+headers = {
+    "Accept": "*/*",
+"User-Agent" :"Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
+}
 url = "http://mebel-saa.ru/"
-soup =
+
+req = requests.get(url,headers=headers)
