f4fb1e10470d707ec9c009ac0b4ccac1f499faea
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 <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>
2021-12-20 00:46:29 +0300

    a half of a job

--- saa_parse.py
+++ saa_parse.py
@@ -7,7 +7,7 @@ import lxml
 #     "Accept": "*/*",
 # "User-Agent" :"Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
 # }
-# url = "http://mebel-saa.ru/"
+url = "http://mebel-saa.ru/"
 #
 # req = requests.get(url,headers=headers)
 # src = req.text
@@ -15,8 +15,32 @@ import lxml
 # with open("index.html","w") as file:
 #     file.write(src)
 
-with open("index.html") as files:
+with open("index.html") as file:
     src = file.read()
+
+soup = BeautifulSoup(src,"lxml")
+
+all_hrefs = soup.find_all("a")
+
+all_product_hrefs = []
+
+# for item in all_hrefs:
+    # print(count,item.text)
+
+all_product_hrefs = all_hrefs[5:43]
+all_products_links = []
+all_product_images = []
+count = 0
+
+for item in all_product_hrefs:
+    item_text = item.text
+    item_href = item.get("href")
+    count+=1
+    print(f"{count}-{item_text}{url + item_href}")
+
+
+
+
 
 
 
