323b85c0c54794405277e0952c153c3e93665e21
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 <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>
2021-12-20 01:45:42 +0300

    data/index add

--- saa_parse.py
+++ saa_parse.py
@@ -4,10 +4,10 @@ import lxml
 import json
 
 
-# headers = {
-#     "Accept": "*/*",
-# "User-Agent" :"Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
-# }
+headers = {
+    "Accept": "*/*",
+"User-Agent" :"Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
+}
 url = "http://mebel-saa.ru"
 #
 # req = requests.get(url,headers=headers)
@@ -16,34 +16,55 @@ url = "http://mebel-saa.ru"
 # with open("index.html","w") as file:
 #     file.write(src)
 
-with open("index.html") as file:
-    src = file.read()
+# with open("index.html") as file:
+#     src = file.read()
+#
+# soup = BeautifulSoup(src,"lxml")
+#
+# all_hrefs = soup.find_all("a")
+#
+# all_product_hrefs = []
+#
+# # for item in all_hrefs:
+#     # print(count,item.text)
+#
+# all_product_hrefs = all_hrefs[5:43]
+# all_products_links = []
+# all_product_images = []
+# count = 0
+#
+# all_categories_dict = {}
+#
+# for item in all_product_hrefs:
+#     item_text = item.text
+#     item_href = item.get("href")
+#
+#     all_categories_dict[item_text] = url+item_href
+#     count+=1
+#
+# with open("all_categories_dict.json", "w") as file:
+#     json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
 
-soup = BeautifulSoup(src,"lxml")
+with open("all_categories_dict.json") as file:
+    all_categories = json.load(file)
 
-all_hrefs = soup.find_all("a")
-
-all_product_hrefs = []
-
-# for item in all_hrefs:
-    # print(count,item.text)
-
-all_product_hrefs = all_hrefs[5:43]
-all_products_links = []
-all_product_images = []
 count = 0
+for category_name,category_href in all_categories.items():
+    if count ==0:
+        rep = [","," ","-"]
+        for item in rep:
+            if item in category_name:
+                category_name = category_name.replace(item, "_")
 
-all_categories_dict = {}
+        req = requests.get(url=category_href,headders=headers)
+        src = req.text
+        with open(f"data/{category_name}.html", "w") as file:
+            file.write(src)
+        count+=1
 
-for item in all_product_hrefs:
-    item_text = item.text
-    item_href = item.get("href")
 
-    all_categories_dict[item_text] = url+item_href
-    count+=1
 
-with open("all_categories_dict.json", "w") as file:
-    json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
+
 
 
 
