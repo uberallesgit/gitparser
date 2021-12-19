31dd691b9c9a759bdef6fb68dc86e26121275091
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 <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>
2021-12-20 00:50:43 +0300

    a half of a job

--- saa_parse.py
+++ saa_parse.py
@@ -7,7 +7,7 @@ import lxml
 #     "Accept": "*/*",
 # "User-Agent" :"Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
 # }
-url = "http://mebel-saa.ru/"
+url = "http://mebel-saa.ru"
 #
 # req = requests.get(url,headers=headers)
 # src = req.text
@@ -32,11 +32,15 @@ all_products_links = []
 all_product_images = []
 count = 0
 
+all_categories_dict = {}
+
 for item in all_product_hrefs:
     item_text = item.text
     item_href = item.get("href")
     count+=1
-    print(f"{count}-{item_text}{url + item_href}")
+    print(f"{count}-{item_text} {url + item_href}")
+
+
 
 
 
